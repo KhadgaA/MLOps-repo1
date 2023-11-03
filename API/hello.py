169e9ac3739d05197fd72988d506fc8c93827a38
KhadgaA <khadgajyothalli@gmail.com>
2023-11-03 15:22:38 +0530

    flask api

---
+++ API/hello.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/hello/<name>")
+def hello_world(name):
+    return f"<p>Hello, World! user {name}</p>"
+
+@app.route("/sum/<name>",method = ["POST"])
+def sum_num():
+    js = request.get_json()
+    x = js["x"]
+    y = js["y"]
+    return f"<p>Hello, user {name}, sum is {x + y}</p>"
