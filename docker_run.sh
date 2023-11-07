c0f4ac149760ad5934484ae7e1395b077734d892
KhadgaA <khadgajyothalli@gmail.com>
2023-11-07 16:13:40 +0530

    docker_assignment

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 docker build -t digits:v1 -f docker/Dockerfile .
 echo "Running Docker image..."
 docker run -d -v "$(pwd)/models/":/digits/models/ digits:v1
-echo "Completed Run ..."
+echo "Completed Run ..."0
