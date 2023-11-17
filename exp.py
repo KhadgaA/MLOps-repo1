c22d06c2d4c74c488d238fe91138a90a93337183
KhadgaA <khadgajyothalli@gmail.com>
2023-11-17 23:26:11 +0530

    updated

--- exp.py
+++ exp.py
@@ -22,10 +22,7 @@ args = parser.parse_args()
 digits = datasets.load_digits()
 
 X, y = preprocess(digits)
-for i, j in zip(X,y):
-    if y ==1:
-        print(i,j)
-        break
+
 
 print("Total number of samples: ",len(X))
 
